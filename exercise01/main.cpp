241beddbaeab9ecbb1ebbc2c1e83916ee9835d6b
tamames <arcospgt@gmail.com>
2025-12-24 14:37:51 +0100

    feature: first exercise and setup

---
+++ exercise01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <omp.h>
+
+int main()
+{
+    #pragma omp parallel
+    {
+        int id = omp_get_thread_num();
+        std::cout << "Hello " << id;
+        std::cout << " world " << id << "\n";
+    }
+
+    std::cout << "End" << std::endl;
+}
