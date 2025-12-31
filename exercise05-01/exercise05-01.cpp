4215236b87f6e056206924ebdea5e100baf01334
tamames <arcospgt@gmail.com>
2025-12-31 14:41:52 +0100

    feature: minimal integration program

---
+++ exercise05-01/exercise05-01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <omp.h>
+
+#define NUM_THREADS 4
+
+int main(int argc, char* argv[]) {
+
+  int num_steps = std::stoi(argv[1]);
+
+  int i;
+  double sum  = 0.0;
+  double step = 1.0 / (double)num_steps;
+  double x;
+
+  omp_set_num_threads(NUM_THREADS);
+
+#pragma omp parallel for private(x) reduction(+ : sum)
+  for (i = 0; i < num_steps; i++) {
+    x   = (i + 0.5) * step;
+    sum = sum + 4.0 / (1.0 + x * x);
+  }
+  double pi = step * sum;
+  std::cout << "Our value is: " << pi << std::endl;
+}
