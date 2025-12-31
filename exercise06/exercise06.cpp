f109c23d806fbd7a9b2794d6230964a4a9ca6a3a
tamames <arcospgt@gmail.com>
2025-12-31 16:30:45 +0100

    feauture: wrong iteration of linked list

---
+++ exercise06/exercise06.cpp
@@ -0,0 +1,102 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef N
+#define N 5
+#endif
+#ifndef FS
+#define FS 38
+#endif
+
+struct node {
+  int data;
+  int fibdata;
+  struct node* next;
+};
+
+int fib(int n) {
+  int x, y;
+  if (n < 2) {
+    return (n);
+  } else {
+    x = fib(n - 1);
+    y = fib(n - 2);
+    return (x + y);
+  }
+}
+
+void processwork(struct node* p) {
+  int n;
+  n          = p->data;
+  p->fibdata = fib(n);
+}
+
+struct node* init_list(struct node* p) {
+  int i;
+  struct node* head = NULL;
+  struct node* temp = NULL;
+
+  head       = (struct node*)malloc(sizeof(struct node));
+  p          = head;
+  p->data    = FS;
+  p->fibdata = 0;
+  for (i = 0; i < N; i++) {
+    temp       = (struct node*)malloc(sizeof(struct node));
+    p->next    = temp;
+    p          = temp;
+    p->data    = FS + i + 1;
+    p->fibdata = i + 1;
+  }
+  p->next = NULL;
+  return head;
+}
+
+int main(int argc, char* argv[]) {
+  double start, end;
+  struct node* p    = NULL;
+  struct node* temp = NULL;
+  struct node* head = NULL;
+
+  printf("Process linked list\n");
+  printf("  Each linked list node will be processed by function 'processwork()'\n");
+  printf("  Each ll node will compute %d fibonacci numbers beginning with %d\n", N, FS);
+
+  head = init_list(p);
+
+  // in order to parallelize this we need to know the number of iterations
+  p = head;
+  int count{0};
+  while (p != NULL) {
+    p = p->next;
+    count++;
+  }
+  p = head;
+
+  // now that we know the number of elements we store a pointer to each element in an array
+  node* parr[count];
+  for (int i = 0; i < count; i++) {
+    parr[i] = p;
+    p       = p->next;
+  }
+
+  start = omp_get_wtime();
+#pragma omp parallel for
+  for (int i = 0; i < count; i++) {
+    processwork(parr[i]);
+  }
+
+  end = omp_get_wtime();
+  p   = head;
+  while (p != NULL) {
+    printf("%d : %d\n", p->data, p->fibdata);
+    temp = p->next;
+    free(p);
+    p = temp;
+  }
+  free(p);
+
+  printf("Compute Time: %f seconds\n", end - start);
+
+  return 0;
+}
