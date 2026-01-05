3e845b6e699de6e6016e36502334ba909b14b6c0
tamames <arcospgt@gmail.com>
2026-01-05 16:40:19 +0100

    feature: add exercise 09

--- exercise09/random.h
+++ exercise09/random.h
@@ -1,2 +1,15 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 double drandom();
 void seed(double low_in, double hi_in);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
